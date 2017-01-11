05c5ee836e330fececa6a96dbc1f24091242bd8a
dcman <justin.johnson.90@my.csun.edu>
2017-01-10 20:51:59 -0800

    Added working back button

--- app/src/main/java/com/fancylancy/android/blackout/MainActivity.java
+++ app/src/main/java/com/fancylancy/android/blackout/MainActivity.java
@@ -221,4 +221,15 @@ public class MainActivity extends AppCompatActivity {
         mHideHandler.postDelayed(mHideRunnable, delayMillis);
     }
 
+    public void backTrack(View view) {
+        Log.i(TAG, "backTrack: Clicked");
+        Intent i = new Intent(Intent.ACTION_MEDIA_BUTTON);
+        synchronized (this) {
+            i.putExtra(Intent.EXTRA_KEY_EVENT, new KeyEvent(KeyEvent.ACTION_DOWN, KeyEvent.KEYCODE_MEDIA_PREVIOUS));
+            sendOrderedBroadcast(i, null);
+
+            i.putExtra(Intent.EXTRA_KEY_EVENT, new KeyEvent(KeyEvent.ACTION_UP, KeyEvent.KEYCODE_MEDIA_PREVIOUS));
+            sendOrderedBroadcast(i, null);
+        }
+    }
 }
